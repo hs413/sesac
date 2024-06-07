c90f71bf72093f302e783d0638117bed6ed20948
rtg <rtg1539@naver.com>
2024-06-07 10:16:59 +0900

    session login, logout

--- java-web/w2/src/main/java/org/zerock/w2/controller/TodoRegisterController.java
+++ java-web/w2/src/main/java/org/zerock/w2/controller/TodoRegisterController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
 import org.zerock.w2.dto.TodoDTO;
 import org.zerock.w2.service.TodoService;
@@ -23,6 +24,22 @@ public class TodoRegisterController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("/todo/register GET ...");
 
+        HttpSession session = req.getSession();
+
+        if (session.isNew()) {
+            log.info("JSESSIONID 쿠키가 새로 만들어짐");
+            resp.sendRedirect("/login");
+            return;
+        }
+
+        // JSESSIONID는 있지만 해당 세션 컨텍스트에 loginInfo라는 이름으로 저장된 객체가 없는 경우
+        if (session.getAttribute("loginInfo") == null) {
+            log.info("로그인 정보가 없는 사용자");
+            resp.sendRedirect("/login");
+            return;
+        }
+
+        // 정상인 경우
         req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req, resp);
     }
 
--- java-web/w2/src/main/webapp/WEB-INF/todo/list.jsp
+++ java-web/w2/src/main/webapp/WEB-INF/todo/list.jsp
@@ -24,5 +24,8 @@
 
     </c:forEach>
 </ul>
+<form action="/logout" method="post">
+    <button>LOGOUT</button>
+</form>
 </body>
 </html>
