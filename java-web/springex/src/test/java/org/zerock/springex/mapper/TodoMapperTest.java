a8261d12af8acfe18ca8831609b767412edb8f0e
rtg <rtg1539@naver.com>
2024-06-09 21:56:17 +0900

    스프링 Web Mvc 구현 세팅

---
+++ java-web/springex/src/test/java/org/zerock/springex/mapper/TodoMapperTest.java
@@ -0,0 +1,23 @@
+package org.zerock.springex.mapper;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
+class TodoMapperTest {
+    @Autowired(required = false)
+    private TodoMapper todoMapper;
+
+    @Test
+    public void getGetTime() {
+        log.info(todoMapper.getTime());
+    }
+}
