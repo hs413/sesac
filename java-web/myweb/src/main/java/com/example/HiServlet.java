cdc82d58010ad8fea1b313a25dac75681a51b4e7
rtg <rtg1539@naver.com>
2024-06-12 11:28:24 +0900

    메이븐 프로젝트2

--- java-web/myweb/src/main/java/com/example/HiServlet.java
+++ java-web/myweb/src/main/java/com/example/HiServlet.java
@@ -1,20 +1,21 @@
 package com.example;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class HelloServlet extends HttpServlet {
+@WebServlet("/hi-servlet")
+public class HiServlet extends HttpServlet {
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html; charset=UTF-8");
         PrintWriter out = resp.getWriter();
-        String name = "SESAC";
-        out.println("<h2>Hello, Servlet</h2>");
+        out.println("Hi Servlet");
         out.close();
     }
 }
---
+++ java-web/myweb/src/main/webapp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: D
+  Date: 2024-06-12
+  Time: 오전 11:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h2>Hello.jsp world!!!!!!</h2>
+</body>
+</html>
