defe97971820c65b2ce265603c2fdbf9e9fb9b9b
rtg <rtg1539@naver.com>
2024-06-14 12:43:07 +0900

    댓글 CRUD

--- java-web/b01/src/main/java/org/zerock/b01/controller/advice/CustomRestAdvice.java
+++ java-web/b01/src/main/java/org/zerock/b01/controller/advice/CustomRestAdvice.java
@@ -1,6 +1,8 @@
 package org.zerock.b01.controller.advice;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -8,13 +10,21 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.View;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 @Log4j2
 public class CustomRestAdvice {
+    private final View error;
+
+    public CustomRestAdvice(View error) {
+        this.error = error;
+    }
+
     @ExceptionHandler(BindException.class)
     @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
     public ResponseEntity<Map<String, String>> handleBindException(BindException e) {
@@ -30,6 +40,32 @@ public class CustomRestAdvice {
             });
         }
 
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleFKException(Exception e) {
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("time", "" + System.currentTimeMillis());
+        errorMap.put("msg", "constraint fails");
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+
+    @ExceptionHandler({
+            NoSuchElementException.class,
+            EmptyResultDataAccessException.class })
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleNoSuchElement(Exception e) {
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("time", "" + System.currentTimeMillis());
+        errorMap.put("msg", "No Such Element Exception");
         return ResponseEntity.badRequest().body(errorMap);
     }
 }
