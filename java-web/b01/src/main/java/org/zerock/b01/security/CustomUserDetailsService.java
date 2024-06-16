ee0484ff84bd4af0c2701c19514ee242ebb02c06
rtg <rtg1539@naver.com>
2024-06-16 12:18:30 +0900

    security member

--- java-web/b01/src/main/java/org/zerock/b01/security/CustomUserDetailsService.java
+++ java-web/b01/src/main/java/org/zerock/b01/security/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package org.zerock.b01.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,29 +10,62 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.zerock.b01.domain.Member;
+import org.zerock.b01.dto.MemberSecurityDTO;
+import org.zerock.b01.repository.MemberRepository;
+
+import java.util.Optional;
 
 @Log4j2
 @Service
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
 
-    public CustomUserDetailsService() {
-        this.passwordEncoder = new BCryptPasswordEncoder();
-    }
+//    private PasswordEncoder passwordEncoder;
+
+//    public CustomUserDetailsService() {
+//        this.passwordEncoder = new BCryptPasswordEncoder();
+//    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("loadUserByUsername: " + username);
 
-        UserDetails userDetails = User.builder()
-                .username("user1")
-//                .password("1111")
-                .password(passwordEncoder.encode("1111"))
-                .authorities("ROLE_USER")
-                .build();
+        Optional<Member> result = memberRepository.getWithRoles(username);
 
-        return userDetails;
+        if (result.isEmpty()) {
+            throw new UsernameNotFoundException("username not found");
+        }
+
+        Member member = result.get();
+
+        MemberSecurityDTO memberSecurityDTO =
+                new MemberSecurityDTO(
+                        member.getMid(),
+                        member.getMpw(),
+                        member.getEmail(),
+                        member.isDel(),
+                        false,
+                        member.getRoleSet().stream().map(
+                                memberRole -> new SimpleGrantedAuthority("ROLE_" + memberRole.name())
+                        ).toList()
+                );
+
+        log.info("memberSecurityDTO");
+        log.info(memberSecurityDTO);
+
+        return memberSecurityDTO;
+
+
+//        UserDetails userDetails = User.builder()
+//                .username("user1")
+//                .password("1111")
+//                .password(passwordEncoder.encode("1111"))
+//                .authorities("ROLE_USER")
+//                .build();
+//
+//        return userDetails;
     }
 }
