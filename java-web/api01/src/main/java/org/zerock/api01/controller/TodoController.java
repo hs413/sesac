52d684b90354d89f96180fb33ccd01b44698ed5e
rtg <rtg1539@naver.com>
2024-06-23 15:10:29 +0900

    todo api 등록, 조회

---
+++ java-web/api01/src/main/java/org/zerock/api01/controller/TodoController.java
@@ -0,0 +1,35 @@
+package org.zerock.api01.controller;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.api01.dto.TodoDTO;
+import org.zerock.api01.service.TodoService;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/todo")
+@Log4j2
+@RequiredArgsConstructor
+@SecurityRequirement(name = "Authorization")
+public class TodoController {
+    private final TodoService todoService;
+
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Long> register(@RequestBody TodoDTO todoDTO) {
+        log.info(todoDTO);
+
+        Long tno = todoService.register(todoDTO);
+
+        return Map.of("tno", tno);
+    }
+
+    @GetMapping("/{tno}")
+    public TodoDTO read(@PathVariable("tno") Long tno) {
+        log.info("read tno: " + tno);
+        return todoService.read(tno);
+    }
+}
