e51f071afd65091c91d34def607c50d83c7ffb97
rtg <rtg1539@naver.com>
2024-06-23 16:27:38 +0900

    todo api 리스트, 수정, 삭제

--- java-web/api01/src/main/java/org/zerock/api01/service/TodoService.java
+++ java-web/api01/src/main/java/org/zerock/api01/service/TodoService.java
@@ -1,6 +1,8 @@
 package org.zerock.api01.service;
 
 import jakarta.transaction.Transactional;
+import org.zerock.api01.dto.PageRequestDTO;
+import org.zerock.api01.dto.PageResponseDTO;
 import org.zerock.api01.dto.TodoDTO;
 
 @Transactional
@@ -9,5 +11,9 @@ public interface TodoService {
 
     TodoDTO read(Long tno);
 
+    PageResponseDTO<TodoDTO> list(PageRequestDTO pageRequestDTO);
 
+    void remove(Long tno);
+
+    void modify(TodoDTO todoDTO);
 }
