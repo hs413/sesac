b4b8785df4133adfa9daffdbddbfcbfbb9d76383
rtg <rtg1539@naver.com>
2024-06-29 20:58:53 +0900

    질문 등록 기능

--- sbb/src/main/java/com/example/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/example/sbb/answer/AnswerController.java
@@ -2,6 +2,7 @@ package com.example.sbb.answer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.sbb.question.Question;
 import com.example.sbb.question.QuestionService;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/answer")
@@ -24,9 +27,16 @@ public class AnswerController {
 	public String creatAnswer(
 			Model model,
 			@PathVariable("id") Integer id,
-			@RequestParam(value = "content") String content) {
+			@Valid AnswerForm answerForm,
+			BindingResult bindingResult) {
 		Question question = this.questionService.getQuestion(id);
-		this.answerService.create(question, content);
+		
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("question", question);
+			return "question_detail";
+		}
+		
+		this.answerService.create(question, answerForm.getContent());
 		return String.format("redirect:/question/detail/%s", id);
 	}
 
