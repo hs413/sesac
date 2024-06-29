69b5d8e347bdb7922b22bd57a98e759d8f49b994
rtg <rtg1539@naver.com>
2024-06-29 16:27:58 +0900

    repository test

---
+++ sbb/src/test/java/com/example/sbb/AnswerRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.example.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import jakarta.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+public class AnswerRepositoryTest {
+	
+	@Autowired
+	private QuestionRepository questionRepository;
+	
+	@Autowired
+	private AnswerRepository answerRepository;
+	
+//	@Test
+//	void testInsert() {
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		
+//		Answer a = new Answer();
+//		a.setContent("네 자동으로 생성됩니다.");
+//		a.setQuestion(q);
+//		a.setCreateDate(LocalDateTime.now());
+//		this.answerRepository.save(a);
+//	}
+	
+	@Test
+	void testRead() {
+		Optional<Answer> oa = this.answerRepository.findById(1);
+		assertTrue(oa.isPresent());
+		Answer a = oa.get();
+		assertEquals(2, a.getQuestion().getId());
+	}
+	
+	@Test
+	@Transactional
+	void testReadByQuestion() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		
+		List<Answer> answerList = q.getAnswerList();
+		
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	}
+}
--- sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.sbb;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -31,13 +33,64 @@ class SbbApplicationTests {
 //		
 //	}
 	
-	@Test
-	void testJpa() {
-		List<Question> all = this.questionRepository.findAll();
-		assertEquals(2, all.size());
-		
-		Question q = all.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
-	}
-
+//	@Test
+//	void testJpa() {
+//		List<Question> all = this.questionRepository.findAll();
+//		assertEquals(2, all.size());
+//		
+//		Question q = all.get(0);
+//		assertEquals("sbb가 무엇인가요?", q.getSubject());
+//	}
+	
+//	@Test
+//	void testJpa() {
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		
+//		if (oq.isPresent()) {
+//			Question q = oq.get();
+//			assertEquals("sbb가 무엇인가요?", q.getSubject());
+//		}
+//	}
+//	
+//	@Test
+//	void testFindBySubject() {
+//		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+//		assertEquals(1, q.getId());
+//	}
+//
+//	@Test
+//	void testFindBySubjectAndContent() {
+//		Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+//		assertEquals(1, q.getId());
+//	}
+//	
+//	@Test
+//	void testFindBySubjectLike() {
+//		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+//		
+//		Question q = qList.get(0);
+//		assertEquals("sbb가 무엇인가요?", q.getSubject());
+//		
+//	}
+	
+//	@Test
+//	void testUpdate() {
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		q.setSubject("수정된 제목");
+//		this.questionRepository.save(q);
+//	}
+	
+//	@Test
+//	void testDelete() {
+//		assertEquals(2, this.questionRepository.count());
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		
+//		this.questionRepository.delete(q);
+//		assertEquals(1, this.questionRepository.count());
+//	}
 }
