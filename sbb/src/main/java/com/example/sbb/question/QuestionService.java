bdd78391de65a8dab662409795b6f289b7191944
rtg <rtg1539@naver.com>
2024-07-06 21:44:47 +0900

    검색 기능

--- sbb/src/main/java/com/example/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/example/sbb/question/QuestionService.java
@@ -10,18 +10,47 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.example.sbb.DataNotFoundException;
+import com.example.sbb.answer.Answer;
 import com.example.sbb.user.SiteUser;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
 	private final QuestionRepository questionRepository;
+	
+	private Specification<Question> search(String kw) {
+		return new Specification<>() {
+			private static final long serifalVersionUID = 1L;
+			
+			@Override
+			public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				query.distinct(true);
 
+				Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+				Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+				Join<Question, SiteUser> u2 = q.join("author", JoinType.LEFT);
+				
+				return cb.or(cb.like(q.get("subject"), "%" + kw + "%"), 
+							cb.like(q.get("content"), "%" + kw + "%"), 
+							cb.like(u1.get("username"), "%" + kw + "%"), 
+							cb.like(a.get("content"), "%" + kw + "%"), 
+							cb.like(u2.get("username"), "%" + kw + "%")); 
+            }
+		};
+	}
+	
 	public List<Question> getList() {
 		return this.questionRepository.findAll();
 	}
@@ -45,11 +74,13 @@ public class QuestionService {
 		this.questionRepository.save(q);
 	}
 	
-	public Page<Question> getList(int page) {
+	public Page<Question> getList(int page, String kw) {
 		List<Sort.Order> sorts = new ArrayList<>();
 		sorts.add(Sort.Order.desc("createDate"));
 		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-		return this.questionRepository.findAll(pageable);
+		return this.questionRepository.findAllByKeyword(kw, pageable);
+//		Specification<Question> spec = search(kw);
+//		return this.questionRepository.findAll(spec, pageable);
 	}
 	
 	public void modify(Question question, String subject, String content) {
